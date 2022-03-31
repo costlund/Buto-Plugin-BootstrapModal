98a1fd01db872003ad58cd1a948107076c55395a
Claes Ö <>
2022-03-31 17:50:06 +0200

    1.1.0

--- PluginBootstrapModal.php
+++ PluginBootstrapModal.php
@@ -1,8 +1,11 @@
 <?php
 class PluginBootstrapModal{
   public function widget_modal($data){
+    $wdata = new PluginWfArray(array('size' => 'lg'));
+    $wdata->merge($data['data']);
+    $wdata->set('dialog_class', 'modal-dialog modal-'.$wdata->get('size'));
     $element = new PluginWfYml(__DIR__.'/element/modal.yml');
-    $element->setByTag($data['data'], 'rs', true);
+    $element->setByTag($wdata->get(), 'rs', true);
     wfDocument::renderElement($element->get());
   }
 }
