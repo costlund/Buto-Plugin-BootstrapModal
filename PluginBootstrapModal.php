163f8aa83c5b7b49d407998f46d0bea23d2bc243
Claes Östlund <>
2022-10-10 16:09:53 +0200

    1.3.0

--- PluginBootstrapModal.php
+++ PluginBootstrapModal.php
@@ -4,6 +4,7 @@ class PluginBootstrapModal{
     $wdata = new PluginWfArray(array('size' => 'lg', 'btn_close' => true));
     $wdata->merge($data['data']);
     $wdata->set('dialog_class', 'modal-dialog modal-'.$wdata->get('size'));
+    $wdata->set('id_body', $wdata->get('id').'_body');
     $element = new PluginWfYml(__DIR__.'/element/modal.yml');
     $element->setByTag($wdata->get(), 'rs', true);
     wfDocument::renderElement($element->get());
